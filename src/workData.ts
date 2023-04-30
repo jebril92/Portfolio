a9360ef456d61c373f9755ca5a46263cd6ba032c
Jebril92 <hocine.jebril@orange.fr>
2023-04-30 19:36:29 +0200

    List Modification

--- src/workData.ts
+++ src/workData.ts
@@ -9,7 +9,7 @@ const projects = [
     title: "CESIndeed",
     techTitle: "Web (Laravel)",
     description: "CESIndeed is a job posting site to make it easier for students to find jobs",
-    repoLink: "https://github.com/jebril92/CESI-Web"
+    repoLink: "https://github.com/jebril92/CESIndeed"
   }
 ];
 
